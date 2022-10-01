dcaca16bbc975c55ab0ca4c7699d4d807e753e76
ElBrazilian <nathan.schiffmacher@gmail.com>
2022-10-01 14:38:01 +0200

    Adding BE1

---
+++ BE1/src/airlines/TravelAgency.java
@@ -0,0 +1,27 @@
+package airlines;
+
+public class TravelAgency {
+
+   private String name;
+   // TODO: complete here
+
+   public TravelAgency(String name) {
+      this.name = name;
+      // TODO: complete here
+   }
+
+   public void addAirline(Airline airline) {
+      // TODO: complete here
+   }
+
+   public Airline getAirlineByIata(String iata) {
+      // TODO: modify line below
+      return null;
+   }
+
+   public void display() {
+      System.out.println(this.name);
+      System.out.println("Suppliers:");
+      // TODO: complete here
+   }
+}
